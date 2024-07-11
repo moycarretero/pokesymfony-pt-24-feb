d729da41c34461e076fa903e47ccdaafbfc1011a
anderplacencia <anderson.jose.placencia@bootcamp-upgrade.com>
2024-07-11 21:26:33 +0200

    muestra un listado de pokemons

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -18,4 +18,29 @@ class PokemonController extends AbstractController{
 
         return $this->render('pokemons/pokemon.html.twig', ['pokemon' => $pokemon]);
     }
+
+    #[Route('/pokemons')]
+    public function pokemons () {
+        $pokemons = 
+        [
+            [ 'name' => 'pikachu',
+            'type' => 'electric',
+            'code' => '838',
+            'description' => 'Es un pokemon amarillo que lanza rayos',
+            'img' => 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png'],
+            
+            [ 'name' => 'Frosmoth',
+            'type' => 'electric',
+            'code' => '0873',
+            'description' => 'Es un pokemon blanco',
+            'img' => 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/873.png'],
+            
+            [ 'name' => 'Charmander ',
+            'type' => 'electric',
+            'code' => '0004',
+            'description' => 'Es un pokemon amarillo que lanza fuego',
+            'img' => 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png']
+        ];
+        return $this->render('pokemons/pokemonsList.html.twig', ['pokemons' => $pokemons]);
+    }
 };
