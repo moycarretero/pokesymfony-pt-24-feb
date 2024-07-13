4a8250b2c3e9e1ab367cff1b0463e7daef4bd7d1
Adrian Fernandez <adriapapo@gmail.com>
2024-07-13 13:36:35 +0200

    Relaciones entre tablas

---
+++ src/Controller/PokemonController.php
@@ -0,0 +1,77 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Entity\Debilidad;
+use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PokemonController extends AbstractController{
+    #[Route('/pokemon/{id}', name: 'showPokemon')] 
+    public function getPokemon(EntityManagerInterface $doctrine, $id){
+        $repository = $doctrine -> getRepository(Pokemon::class);
+        $pokemon = $repository -> find($id);
+        
+
+        return $this->render('pokemons/pokemon.html.twig', ['pokemon' => $pokemon]);
+    }
+
+    #[Route('/pokemons', name:'listPokemons')]
+    public function pokemons (EntityManagerInterface $doctrine) {
+       $repository = $doctrine -> getRepository(Pokemon::class);
+        $pokemons = $repository -> findAll();
+
+
+        return $this->render('pokemons/pokemonsList.html.twig', ['pokemons' => $pokemons]);
+    }
+    #[Route('/new/pokemons')]
+    public function newPokemons (EntityManagerInterface $doctrine) {
+    
+        $pokemon = new Pokemon();
+        $pokemon -> setNombre('pikachu');
+        $pokemon -> setDescription('Es un pokemon amarillo que lanza rayos');
+        $pokemon -> setImage('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png');
+        $pokemon -> setCode(838);
+
+        $pokemon2 = new Pokemon();
+        $pokemon2 -> setNombre('Frosmoth');
+        $pokemon2 -> setDescription('Es un pokemon amarillo que lanza rayos');
+        $pokemon2 -> setImage('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/873.png');
+        $pokemon2 -> setCode(838);
+
+        $pokemon3 = new Pokemon();
+        $pokemon3 -> setNombre('Charmander');
+        $pokemon3 -> setDescription('Es un pokemon amarillo que lanza rayos');
+        $pokemon3 -> setImage('https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png');
+        $pokemon3 -> setCode(838);
+
+
+        $debilidad = new Debilidad();
+        $debilidad -> setName('fuego');
+
+
+        $debilidad2 = new Debilidad();
+        $debilidad2 -> setName('agua');
+
+
+        $debilidad3 = new Debilidad();
+        $debilidad3 -> setName('aire');
+        $pokemon -> addDebilidade($debilidad);
+        $pokemon -> addDebilidade($debilidad2);
+        $pokemon2 -> addDebilidade($debilidad);
+        $pokemon3 -> addDebilidade($debilidad3);
+
+        $doctrine -> persist($pokemon);
+        $doctrine -> persist($pokemon2);
+        $doctrine -> persist($pokemon3);
+        $doctrine -> persist($debilidad);
+        $doctrine -> persist($debilidad2);
+        $doctrine -> persist($debilidad3);
+        $doctrine -> flush();
+
+        return new Response('Pokemons insertados correctamente');
+    }
+};
---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokemonRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PokemonRepository::class)]
+class Pokemon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column]
+    private ?int $code = null;
+
+    /**
+     * @var Collection<int, Debilidad>
+     */
+    #[ORM\ManyToMany(targetEntity: Debilidad::class, inversedBy: 'poekmons')]
+    private Collection $debilidades;
+
+    public function __construct()
+    {
+        $this->debilidades = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Debilidad>
+     */
+    public function getDebilidades(): Collection
+    {
+        return $this->debilidades;
+    }
+
+    public function addDebilidade(Debilidad $debilidade): static
+    {
+        if (!$this->debilidades->contains($debilidade)) {
+            $this->debilidades->add($debilidade);
+        }
+
+        return $this;
+    }
+
+    public function removeDebilidade(Debilidad $debilidade): static
+    {
+        $this->debilidades->removeElement($debilidade);
+
+        return $this;
+    }
+}
